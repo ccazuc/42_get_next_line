9bbf4cc89883cf8027c155f3aea0baae5332ba88
Mideas <clementorus@hotmail.fr>
2018-06-02 17:47:37 +0200

    c bon ou pa?

---
+++ get_next_line_old.c
@@ -0,0 +1,277 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccazuc <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/08 09:17:41 by ccazuc            #+#    #+#             */
+/*   Updated: 2018/04/30 13:49:43 by ccazuc           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+static char		*build_line(t_gnl *env)
+{
+	int		i;
+
+	i = -1;
+	env->curr_line = 0;
+	//printf("env->datas: %s\n", env->datas);
+	while (env->datas[++i] && env->end != i)
+		if (env->datas[i] == '\n')
+		{
+			++env->curr_line;
+			if (env->curr_line == env->line)
+				env->start = i;
+			else if (env->curr_line == env->line + 1)
+				env->end = i;
+		}
+	//printf("a\n");
+	printf("i: %d, env: %d, sub: %s\n", i, env->end, ft_strsub_start(env->datas, i));
+	i = i;
+	if (env->curr_line <= env->line || env->end == -1)
+	{
+		env->eof = 1;
+	//	printf("i before: %d\n", i);
+		//i = i;
+		//i = i - 2;
+		printf("i after: %d\n", i);
+		while (i >= 0 && env->datas[i] != '\n')
+			--i;
+		//printf("A\n");
+		//printf("i: %d\n", i);
+		return (i == (int)ft_strlen(env->datas) - 1 ? NULL :
+		ft_strsub_start(env->datas, i + 1));
+	}
+	//printf("b\n");
+	//printf("i: %d, env: %d\n", i, env->end);
+	i = i;
+	env->buff_pos = i + 1;
+	if (env->start == -1)
+	{
+		//env->buff_pos = env->end + 1;
+		return (ft_strsub(env->datas, 0, env->end));
+	}
+	//printf("c\n");
+	env->buff_pos = i + 1;
+	//printf("i: %d, env: %d\n", i, env->end);
+	i = i;
+	return (ft_strsub(env->datas, env->start + 1, env->end - env->start - 1));
+}
+
+static int		add_datas(char *datas, t_gnl *env)
+{
+	int		i;
+
+	if (!(env->datas = ft_strjoin_free1(env->datas, datas)))
+		return (-1);
+	i = -1;
+	env->curr_line = -1;
+	while (env->datas[++i])
+		if (env->datas[i] == '\n')
+		{
+			++env->curr_line;
+			if (env->curr_line == env->line)
+				return (1);
+		}
+	return (0);
+}
+
+static char		*get_n_line(t_gnl *env)
+{
+	int		datas_read;
+	char	buffer[BUFF_SIZE + 1];
+	int		returned_value;
+	char	*tmp;
+
+	env->curr_line = 0;
+	if (env->buff_pos < env->buff_len)
+		if ((tmp = build_line(env)))
+			return (tmp);
+	while ((datas_read = read(env->fd, buffer, BUFF_SIZE)) > 0)
+	{
+		buffer[datas_read] = '\0';
+		env->buff_len += datas_read;
+		returned_value = add_datas(buffer, env);
+		if (returned_value == -1)
+			return (NULL);
+		if (returned_value == 1)
+			return (build_line(env));
+	}
+	env->eof = 1;
+	if (datas_read == -1)
+	{
+		env->eof = 0;
+		return (NULL);
+	}
+	return (build_line(env));
+}
+
+t_gnl			*init_env(t_gnl **env_param, char shouldDelete)
+{
+	t_gnl	*env;
+
+	if (shouldDelete == 1)
+	{
+		if ((*env_param)->eof)
+		{
+			free(*env_param);
+			*env_param = NULL;
+			return (NULL);
+		}
+		return (*env_param);
+	}
+	printf("BLABLABL\n\n\n");
+	if (!(env = malloc(sizeof(*env))))
+		return (NULL);
+	if (!(env->datas = malloc(1)))
+		return (NULL);
+	env->datas[0] = '\0';
+	env->buff_len = 0;
+	env->buff_pos = 0;
+	env->line = -1;
+	env->eof = 0;
+	return (env);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_gnl		*env;
+
+	if (fd < 0 || fd > 255 || !line || (!env && (!(env = init_env(NULL, 0)))))
+		return (-1);
+	if (!init_env(&env, 1))
+		return (0);
+	++env->line;
+	env->fd = fd;
+	env->start = -1;
+	env->end = -1;
+	if (!(*line = get_n_line(env)))
+	{
+		if (!init_env(&env, 1))
+			return (0);
+		return (-1);
+	}
+/*	if (!init_env(&env, 1))
+		return (0);*/
+	if (env->eof)
+	{
+		free(env);
+		env = NULL;
+		return (0);
+	}
+	return (1);
+}
+
+void	mdr(int fd, char *str)
+{
+	write(fd, str, ft_strlen(str));
+}
+
+int		main(int argc, char **argv)
+{
+
+/*	char	*line;
+	int		out;
+	int		p[2];
+	int		fd;
+
+	out = dup(1);
+	pipe(p);
+
+	fd = 1;
+	dup2(p[1], fd);
+	write(fd, "efghijk\nlabcde\n", 15);
+	close(p[1]);
+	dup2(out, fd);
+	int returned = get_next_line(p[0], &line);
+	printf("%d, line: '%s'\n\n\n\n", returned, line);
+	returned = get_next_line(p[0], &line);
+	printf("%d, line: '%s'\n\n\n\n", returned, line);
+	returned = get_next_line(p[0], &line);
+	prindqdqstf("%d, line: '%s'\n\n\n\n", returned, line);
+
+}*/
+
+/*	char	*line;
+	int		fds[2];
+
+	pipe(fds);
+	dup2(fds[0], 1);
+	mdr(fds[1], "qbcdefg\n\n\n\n");
+	close(fds[1]);
+	int returned = 0;
+	while ((returned = get_next_line(1, &line)) == 1)
+		dprintf(2, "ret: %d, line: %s\n", returned, line);
+	returned = get_next_line(1, &line);
+	dprintf(2, "ret: %d, line: %s\n", returned, line);
+	returned = get_next_line(1, &line);
+	dprintf(2, "ret: %d, line: %s\n", returned, line);
+
+	dprintf(2, "\n");
+
+	pipe(fds);
+	dup2(fds[0], 1);
+	mdr(fds[1], "sa\n\n\nlut\n");
+	close(fds[1]);
+	while ((returned = get_next_line(1, &line)) == 1)
+		dprintf(2, "ret: %d, line: %s\n", returned, line);
+	returned = get_next_line(1, &line);
+	dprintf(2, "ret: %d, line: %s\n", returned, line);
+	returned = get_next_line(1, &line);
+	dprintf(2, "ret: %d, line: %s\n", returned, line);
+
+	dprintf(2, "\n");
+}*/
+
+
+/*	char	*line;
+	int		out;
+	int		p[2];
+	char	*str;
+	int		gnl_ret;
+
+	str = (char *)malloc(1000 * 1000);
+	*str = '\0';
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+	out = dup(1);
+	pipe(p);
+	dup2(p[1], 1);
+
+	write(1, str, strlen(str));
+	close(p[1]);
+	dup2(out, 1);
+	gnl_ret = get_next_line(p[0], &line);
+	printf("line: '%s'\n\n\n gnl: '%s'\n\n\ncmp: %d, return: %d\n", str, line, ft_strcmp(line, str), gnl_ret);
+
+}*/
+
+
+	char	*line;
+	int		fd;
+	int		i;
+	int		max;
+	int		returned_value;
+
+	i = -1;
+	fd = open(argv[1], O_RDONLY);
+	max = ft_atoi(argv[2]);
+	while (++i < max && (returned_value = get_next_line(fd, &line)) == 1)
+	{
+		printf("'%d' '%s'\n", returned_value, line);
+		free(line);
+	}
+	returned_value = get_next_line(fd, &line);
+	printf("last: %d\n", returned_value);
+	//printf("GNL: %d, '%s'\n", get_next_line(fd, &line), line);
+	//printf("GNL: %d, '%s'\n", get_next_line(fd, &line), line);
+	//printf("GNL: %d, '%s'\n", get_next_line(fd, &line), line);
+	//printf("GNL: %d, '%s'\n", get_next_line(fd, &line), line);
+	//printf("GNL: %d, '%s'\n", get_next_line(fd, &line), line);
+	return (0);
+}
